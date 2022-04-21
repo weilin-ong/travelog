8478deda0523e140f9982cda2cc930c8cbd6eabd
Wei Lin Ong <ongweilin93@gmail.com>
2022-04-21 21:12:33 +0200

    add google font, implement sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import './Sidebar.scss';
+import logo from '../../images/logo.png';
 
-function Sidebar() {
-  return <div>Sidebar</div>;
+function Sidebar({ markers }) {
+  return (
+    <section className='sidebar'>
+      <div className='sidebar-header'>
+        <img src={logo} alt='travker-logo' className='sidebar-header--logo' />
+      </div>
+      <div className='sidebar-content'>
+        {markers.length
+          ? markers.map((marker) => {
+              return (
+                <div className='sidebar-content--item'>
+                  <p className='sidebar-content--item--place'>
+                    {' '}
+                    {marker.place}{' '}
+                  </p>
+                  <p className='sidebar-content--item--date'> {marker.date} </p>
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <div className='sidebar-logout'>Logout</div>
+    </section>
+  );
 }
 
 export default Sidebar;
