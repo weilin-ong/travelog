d962e68f8d6546e965911f4a5e06161d7f1ed995
Wei Lin Ong <ongweilin93@gmail.com>
2022-04-23 12:22:13 +0200

    delete unused image and add logo to homepage

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Homepage.scss';
+import { ReactComponent as Logo } from '../../images/home-logo-black.svg';
 
 function Homepage() {
   return (
     <section className='home-container'>
-      <Link to='/login'>
-        <button className='login home-container--btn'> login </button>
-      </Link>
+      <Logo className='home-logo' />
+      <div className='homeBtn-container'>
+        <Link to='/login'>
+          <button className='login home-container--btn'> login </button>
+        </Link>
 
-      <Link to='/register'>
-        <button className='register home-container--btn'>register </button>
-      </Link>
+        <Link to='/register'>
+          <button className='register home-container--btn'>register </button>
+        </Link>
+      </div>
     </section>
   );
 }
