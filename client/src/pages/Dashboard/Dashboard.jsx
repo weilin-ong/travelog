3cb3061767ca19e28058390e4fda3d9ffdde18d1
Wei Lin Ong <ongweilin93@gmail.com>
2022-04-30 17:56:35 +0200

    replace google api key

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -44,8 +44,9 @@ function Dashboard() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCnviIn1Z6zyJFI6lBxwlcztjnmChuRxQU',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     //for search places, we want places libraries
     libraries,
   });
